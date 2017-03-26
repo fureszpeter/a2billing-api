3e0794a321587b3c06cf6200699f60ad1012d927
Peter Furesz <fureszpeter@gmail.com>
2017-03-26 23:03:04 +0200

    fix missing update condition from payment

--- app/Domain/Services/PaymentService.php
+++ app/Domain/Services/PaymentService.php
@@ -43,7 +43,13 @@ class PaymentService
 
 
         DB::connection('a2billing')->transaction(function () use ($payment, $subscription, $request) {
-            DB::connection('a2billing')->update('UPDATE cc_card SET credit=credit + ?', [$request->getCreditValue()]);
+            DB::connection('a2billing')->update(
+                'UPDATE cc_card SET credit=credit + :credit_value WHERE id=:subscription_id',
+                [
+                    'credit_value' => $request->getCreditValue(),
+                    'subscription_id' => $subscription->id
+                ]
+            );
 
             $payment->save();
         });
